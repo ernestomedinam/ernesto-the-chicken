55e8882069488cb08ab169256f63ccf4c9cac7d5
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2024-06-12 19:38:16 -0400

    :sparkles: Creates random story intro
    - :sparkles: uses javascript to add initial lines to story.

--- index.js
+++ index.js
@@ -1,28 +1,93 @@
 window.onload = function () {
-    document.body.style.backgroundColor = "#555";
-
-    // build a list of chicken names
-    let chickenNames = ["Sedonia", "Justice", "Ernesto", "Gerardo", "Alison", "Oneil", "Zach"];
-
-    // choose random chicken name
-    const chosenName = chooseRandomItem(chickenNames);
-
-    // build a list of chicken colors
-    let chickenColors = ["pink", "yellow", "blue", "purple", "brown", "red", "white", "orange"];
-
-    // choose random chicken color
-    const chosenColor = chooseRandomItem(chickenColors);
-
-    const chickenHobbies = ["skying", "smoking", "drinking coffee"];
-
-    // build the this is the story of header with the random values 
-    const newHeader = `There was a ${chosenColor} chicken, named ${chosenName}, standing in front of the road...`
-
-    // add it or update in the interface
-    // grab the h1 by its tagname
+    const [
+        chickenNames, 
+        chickenColors, 
+        chickenTraits
+    ] = getInitialData(); 
     const h1 = document.querySelector("h1");
-    // change its text content
-    h1.textContent = newHeader;
+    h1.textContent = buildStoryIntro(
+        chickenNames, 
+        chickenColors, 
+        chickenTraits
+    );
+    document.body.style.backgroundColor = matchColorInString(
+        h1.textContent,
+        chickenColors
+    );
+    const lines = [
+        "The chicken ponders whether to cross or not...",
+        "All of a sudden, a cat appears!!",
+        "The dog came to say hello too",
+        "Then the dog said goodbye and left",
+        "The dog ran into another dog",
+        "The chicken followed the dogs outside"
+    ];
+    const reducedLisString = lines.reduce(
+        (finalString, line, index, copyArr) => {
+            return `${finalString}<li>${line}</li>`
+        }, "" // <- finalString initial value
+    );
+
+    // regular looping way
+    // let lisString = "";
+    // for (const line of lines) {
+    //     // add an '<li> </li>' to the lisString
+    //     lisString += `<li>${line}</li>`;
+    // }
+
+    // grab the ul
+    const ul = document.querySelector("ul");
+    // add string as innerHTML
+    ul.innerHTML = reducedLisString;
+};
+
+function getInitialData() {
+    return [[
+        "Sedonia", 
+        "Justice", 
+        "Ernesto", 
+        "Gerardo", 
+        "Alison", 
+        "Oneil", 
+        "Zach"
+    ], [
+        "pink", 
+        "yellow", 
+        "lightblue", 
+        "violet", 
+        "beige", 
+        "yellowgreen", 
+        "white", 
+        "orange"
+    ], [
+        "cowardly", 
+        "brave", 
+        "smart", 
+        "happy", 
+        "sad", 
+        "funny", 
+        "lazy", 
+        "emotional", 
+        "lonely"
+    ]];
+};
+
+function buildStoryIntro(names, colors, traits) {
+    return `This is the story of a ${
+        chooseRandomItem(traits)
+    }, ${
+        chooseRandomItem(traits)
+    }, ${
+        chooseRandomItem(traits)
+    } ${
+        chooseRandomItem(colors)
+    } chicken, named ${
+        chooseRandomItem(names)
+    }... it all starts on a ${
+        new Date().toLocaleDateString("en-US", {
+            weekday: "long"
+        })
+    } night, while standing in front of the road...`;
 };
 
 function chooseRandomItem(array) {
@@ -30,3 +95,11 @@ function chooseRandomItem(array) {
     return array[randomIndex];
 };
 
+function matchColorInString(str, colors) {
+    const colorsCopy = [...colors]
+    while (colorsCopy.length > 0) {
+        const color = colorsCopy.pop();
+        if (str.includes(color)) return color;
+    };
+    return "white";
+};
